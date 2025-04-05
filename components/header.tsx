b5fa15aee51fdd26231ba48a240bba78f6e8793f
mochiszs <ruaperezmoises@gmail.com>
2025-04-04 20:28:00 -0500

    5

--- components/header.tsx
+++ components/header.tsx
@@ -16,8 +16,14 @@ export default function Header() {
     <header className="bg-[#09465b] text-white">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-20">
-        <Link href="/" className="flex items-center">
-           
+          <Link href="/" className="flex items-center">
+            <Image
+               src="/nuevasfotos/SANNA VET LOGO BLANCO_Mesa de trabajo 1.png"
+              alt="Logo Veterinaria"
+              width={180}
+              height={50}
+              className="h-12 w-auto"
+            />
           </Link>
 
           <button className="md:hidden focus:outline-none" onClick={toggleMenu}>
